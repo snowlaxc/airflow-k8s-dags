b1f045e329fa1c77e6f4f290bba987cc63d35a42
천광현 <ethan1000@bubblecon.io>
2025-03-13 10:51:36 +0900

    json 변환 처리 테스트

--- dags/lrs_statement_extractor_dag.py
+++ dags/lrs_statement_extractor_dag.py
@@ -24,11 +24,18 @@ def execute_sql(sql: str, conn_id: str = 'lrs_connection') -> List[Any]:
     hook = PostgresHook(postgres_conn_id=conn_id)
     # Connection 정보에서 metadata 제외하여 psycopg2에 전달
     conn = hook.get_connection(conn_id)
-    print(type(conn.extra))
-    # extra_dict = json.loads(conn.extra) if conn.extra else {}
-    # filtered_extra = {k: v for k, v in extra_dict.items() if k != 'metadata'}
-    # conn.extra = json.dumps(filtered_extra) if filtered_extra else None
-    # hook.connection = conn
+    try:
+        extra_dict = json.loads(conn.extra)
+        print(extra_dict)
+        print(type(extra_dict))
+        
+        print(extra_dict['metadata'])
+        print(type(extra_dict['metadata']))
+        # filtered_extra = {k: v for k, v in extra_dict.items() if k != 'metadata'}
+        # conn.extra = json.dumps(filtered_extra) if filtered_extra else None
+        # hook.connection = conn
+    except Exception as e:
+        print(e)
     
     # # schema 정보 가져오기
     # schema = conn.schema
