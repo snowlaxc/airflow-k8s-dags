d6ef9e25eab2f9af4afd70634c7a6f000020bcb2
천광현 <ethan1000@bubblecon.io>
2025-03-13 17:21:15 +0900

    test

--- dags/LearningRecord_ETL.py
+++ dags/LearningRecord_ETL.py
@@ -263,8 +263,8 @@ def learning_record_etl():
             SELECT {column_list}
             FROM {config['schema']}.{config['table']}
             WHERE id > {metadata['last_id']}
-            AND timestamp >= '{yesterday.strftime('%Y-%m-%d %H:%M:%S')}'
-            AND timestamp < '{today.strftime('%Y-%m-%d %H:%M:%S')}'
+            AND timestamp >= '2025-01-01 00:00:00'
+            AND timestamp < '2025-02-28 23:59:59'
             ORDER BY id ASC;
         """
         
