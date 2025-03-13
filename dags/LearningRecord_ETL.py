68c32b82f5d2ca4e9c49c67faca8f5ddaf27fbf6
천광현 <ethan1000@bubblecon.io>
2025-03-13 17:21:41 +0900

    test

--- dags/LearningRecord_ETL.py
+++ dags/LearningRecord_ETL.py
@@ -127,7 +127,7 @@ def get_month_day_from_timestamp(timestamp) -> tuple:
     dag_id='LearningRecord_ETL',
     default_args=default_args,
     description='Extract and save LRS statements to JSON files',
-    schedule_interval='0 2 * * *',  # 매일 오전 2시 (KST)
+    schedule_interval='22 17 * * *',  # 매일 오전 2시 (KST)
     catchup=False,
     start_date=datetime(2024, 3, 12, tzinfo=KST_OFFSET)  # KST 기준
 )
